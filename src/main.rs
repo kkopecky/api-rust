506ce15638b3325d587db7588a4cabcef8f3038c
Kai Kopecky <5694288+kkopecky@users.noreply.github.com>
2023-09-19 01:52:30 +0200

    Add htmx button

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_files as fs;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
@@ -14,14 +15,21 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn clicked() -> impl Responder {
+    // print!("Clicked! : {} ", req_body);
+    HttpResponse::Ok().body("<h2>Clicked!</h2>")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    HttpServer::new(|| 
         App::new()
+            .service(fs::Files::new("/static", "./src/static").show_files_listing())
             .service(hello)
             .service(echo)
+            .route("/clicked", web::post().to(clicked))
             .route("/hey", web::get().to(manual_hello))
-    })
+    )
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
